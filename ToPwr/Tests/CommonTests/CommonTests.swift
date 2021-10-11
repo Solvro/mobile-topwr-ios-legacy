c515cc0a617220744300da5f65d209ae1991d227
Jakub Legut <jakub-legut@wp.pl>
2021-10-11 19:19:59 +0200

    hot fix

---
+++ ToPwr/Tests/CommonTests/CommonTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+import Foundation
+
+final class CommonTests: XCTestCase {
+    
+    func test_Example() {
+        XCTAssertEqual(ToPwr().text, "Hello, World!")
+    }
+}
---
+++ ToPwr/Tests/SplashFeatureTests/SplashFeatureTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+import Foundation
+
+final class SplashFeatureTests: XCTestCase {
+    
+    func test_Example() {
+        XCTAssertEqual(ToPwr().text, "Hello, World!")
+    }
+}
