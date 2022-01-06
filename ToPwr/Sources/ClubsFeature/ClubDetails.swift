e25421ad68ba09da0847d494843ac78e6d4bb5f6
Jakub Legut <jakub-legut@wp.pl>
2022-01-06 13:59:38 +0100

    Club feature refactor

--- ToPwr/Sources/ClubsFeature/ClubDetails.swift
+++ ToPwr/Sources/ClubsFeature/ClubDetails.swift
@@ -1,55 +1,92 @@
 import Foundation
 import SwiftUI
 import Common
+import ComposableArchitecture
 
+//MARK: - STATE
+public struct ClubDetailsState: Equatable, Identifiable {
+    public let id: UUID
+    let club: ScienceClub
+    public init(
+        id: UUID = UUID(),
+        club: ScienceClub
+    ){
+        self.id = id
+        self.club = club
+    }
+}
+//MARK: - ACTION
+public enum ClubDetailsAction: Equatable {
+    case onAppear
+}
+
+//MARK: - ENVIRONMENT
+public struct ClubDetailsEnvironment {
+    public init () {}
+}
+
+//MARK: - REDUCER
+public let clubDetailsReducer = Reducer<
+    ClubDetailsState,
+    ClubDetailsAction,
+    ClubDetailsEnvironment
+> { state, action, env in
+    switch action {
+    case .onAppear:
+        return .none
+    }
+}
+//MARK: - VIEW
 public struct ClubDetailsView: View {
-    let club: ClubCellState
+    let store: Store<ClubDetailsState, ClubDetailsAction>
     
     public init(
-        club: ClubCellState
+        store: Store<ClubDetailsState, ClubDetailsAction>
     ) {
-        self.club = club
+        self.store = store
     }
     
     public var body: some View {
-        ScrollView {
-            VStack {
-                ImageView(
-                    url: URL(string: club.club.background?.url ?? ""),
-                    contentMode: .aspectFill
-                )
-                    .frame(height: 300)
-                
-                ImageView(
-                    url: URL(string: club.club.photo?.url ?? ""),
-                    contentMode: .aspectFill
-                )
-                    .frame(width: 120, height: 120)
-                    .clipShape(Circle())
-                    .shadow(radius: 7, x: 0, y: -5)
-                    .offset(y: -60)
-                    .padding(.bottom, -60)
-                
-                Text(club.club.name ?? "")
-                    .font(.appBoldTitle2)
-                    .horizontalPadding(.big)
-                
-                Text(club.club.locale)
-                    .font(.appRegularTitle2)
-                    .horizontalPadding(.huge)
-                
-                ZStack {
-                    VStack {
-                        Text(Strings.Other.deanOffice)
-                            .font(.appBoldTitle2)
+        WithViewStore(store) { viewStore in
+            ScrollView {
+                VStack {
+                    ImageView(
+                        url: URL(string: viewStore.club.background?.url ?? ""),
+                        contentMode: .aspectFill
+                    )
+                        .frame(height: 300)
+                    
+                    ImageView(
+                        url: URL(string: viewStore.club.photo?.url ?? ""),
+                        contentMode: .aspectFill
+                    )
+                        .frame(width: 120, height: 120)
+                        .clipShape(Circle())
+                        .shadow(radius: 7, x: 0, y: -5)
+                        .offset(y: -60)
+                        .padding(.bottom, -60)
+                    
+                    Text(viewStore.club.name ?? "")
+                        .font(.appBoldTitle2)
+                        .horizontalPadding(.big)
+                    
+                    Text(viewStore.club.locale)
+                        .font(.appRegularTitle2)
+                        .horizontalPadding(.huge)
+                    
+                    ZStack {
+                        VStack {
+                            Text(Strings.Other.deanOffice)
+                                .font(.appBoldTitle2)
+                        }
+                        HStack {
+                            Text(viewStore.club.contact.first?.name ?? "")
+                        }
                     }
-                    HStack {
-                        Text(club.club.contact.first?.name ?? "")
-                    }
+                    .background(K.Colors.lightGray)
                 }
-                .background(K.Colors.lightGray)
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
