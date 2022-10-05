a864359b8e3d247432013a7e3a6961d33383020d
Jakub Legut <jakub-legut@wp.pl>
2022-10-05 19:06:37 +0000

    Hotfix/week of year on appear

---
+++ ToPwr/Sources/Common/Extensions/View+Foreground.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+public extension View {
+  func onAppCameToForeground(perform action: @escaping () -> Void) -> some View {
+    self.onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+       action()
+    }
+  }
+
+  func onAppWentToBackground(perform action: @escaping () -> Void) -> some View {
+    self.onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+      action()
+    }
+  }
+}
