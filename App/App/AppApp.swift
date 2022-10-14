05d6f2b09051d249b97341560d795a724066d852
mikolajata <mikolaj.adawaz@gmail.com>
2022-10-14 18:39:19 +0000

    Crashlytics

--- App/App/AppApp.swift
+++ App/App/AppApp.swift
@@ -1,8 +1,22 @@
 import SwiftUI
 import ToPwr
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct AppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             splashView
